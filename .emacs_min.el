78b6c32aff8cb10d358b714abdc9cc691e63cb6e
Darko Draskovic <darko.draskovic@gmail.com>
2021-01-21 16:46:45 +0100

    Rewrite package conf to use use-package
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- .emacs_min.el
+++ .emacs_min.el
@@ -11,39 +11,5 @@
 (setq ido-enable-flex-matching 1)
 (ido-mode t)
 
-(linum-mode 1)
 (electric-pair-mode 1)
 (show-paren-mode 1)
-
-;;;;;;;;;;;;;;;;;;;;;
-;; REQUIRE PACKAGE ;;
-;;;;;;;;;;;;;;;;;;;;;
-;; (require 'package)
-;; (package-initialize) 
-
-;; ;; AUTO-COMPLETE
-;; (require 'auto-complete-config)
-;; (ac-config-default)
-;; (setq ac-auto-start 3)
-
-;; ;; MOVE-TEXT
-;; (require 'move-text)
-;; (move-text-default-bindings)
-
-;; ;; HELM
-;; (require 'helm)
-;; (require 'helm-config)
-;; (helm-mode t)
-
-;; ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
-;; ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
-;; ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-;; (global-set-key (kbd "C-c h") 'helm-command-prefix)
-;; (global-unset-key (kbd "C-x c"))
-
-;; ;; You have to bind helm-M-x to M-x manually. Otherwise, you still get Helm completion, but using the vanilla M-x that does not provides the above features like showing key bindings and TAB to open built-in documentation.
-;; (global-set-key (kbd "M-x") 'helm-M-x)
-;; (setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
-
-;; (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-;; (global-set-key (kbd "M-s o") 'helm-occur)
